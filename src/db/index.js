5d1f23be451a592da28a1c788ea5ed8039bf2a14
kishan saini <kishansaini666@gmail.com>
2025-07-23 21:39:20 +0530

    databasesetuop

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//     app.on("error", () => {
+//       console.log("ERROR: MongoDB connection failed");
+//     });
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//   }
+// })();
