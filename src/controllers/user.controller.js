65018145a05fd45cb6dac2b4cc015f3f8221da90
kishan saini <kishansaini666@gmail.com>
2025-07-31 21:31:26 +0530

    fileupload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,21 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "User registered successfully",
-  });
+  // return res.status(200).json({
+  //   message: "User registered successfully",
+  // });
+  // Simulating user registration logic
+  //all step get user detalis from frontend
+  //vaLIDATE NOT EMPTY
+  //check if user already exists username or email
+  //check for images avtar and coverImage
+  //upload them to cloudinary
+  //create user object- cretae user in database
+  //remove passowrd from  refrwsh tpken field fron response
+  //check for user creation
+  //return success message with user details
+  const { fullname, email, username, passowrd } = req.body;
+  console.log("email:", email);
 });
 
 export { registerUser }; // ✅ Correct: named export
